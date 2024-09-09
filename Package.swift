013068e1274fe59938c506c024e64d4332700882
sandsn <330770597@qq.com>
2024-09-10 02:36:18 +0800

    ffmpeg 7.0

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "LSFFmpegKit",
-    platforms: [.iOS(.v12), .macCatalyst(.v13), .macOS(.v10_13)],
+    platforms: [.iOS(.v13), .macCatalyst(.v14), .macOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
